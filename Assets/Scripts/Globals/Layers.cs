b614e5d1d78c7c85e3421e6a2487a617eb696270
mderu <mderu001@ucr.edu>
2014-10-31 15:49:48 -0700

    Placed things into folders. Cleaned scenes up

---
+++ Assets/Scripts/Globals/Layers.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public static class Layers {
+
+	//Use bitwise funtions to ignore or include layers.
+	//Ex: 
+	//~Player & ~Ground will ignore both the player and the floor
+
+	public const int Default       = 1 << 0;
+	public const int TransparentFX = 1 << 1;
+	public const int IgnoreRaycast = 1 << 2;
+
+	public const int Water         = 1 << 4;
+	public const int UI            = 1 << 5;
+
+	
+	public const int Vehicles      = 1 << 8;
+	public const int Characters    = 1 << 9;
+	public const int Environment   = 1 << 10;
+	public const int Player        = 1 << 11;
+}
---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	RaycastHit CamRayHit(float raycastRange = 50f, int ignoreList = (~Layers.Environment & ~Layers.Player)){
+		RaycastHit camRayHit;
+		Transform camera = transform.parent.GetChild(1).GetChild(0);
+		Physics.Raycast (camera.position, camera.rotation * (Vector3.forward), out camRayHit, raycastRange, ignoreList);
+		return camRayHit;
+	}
+
+}

9660900ae0ff9568a5ac0ba0967d7ca3a3db2674
johnli1101 <jli042@ucr.edu>
2014-11-14 08:42:18 -0800

    Added comments to Enemy Movement So far
    Because comments make everything nice :D.

--- Assets/EnemyMovement.cs
+++ Assets/EnemyMovement.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 public class EnemyMovement : MonoBehaviour {
 	public GameObject target;
-	float speed = 0.07f;
+	float speed = 0.07f;	//speed of the mob
 	bool isFollowing = false;
 	float fRange = 25f;// follow range
 	float aRange = 2.25f;//attack range
@@ -13,33 +13,42 @@ public class EnemyMovement : MonoBehaviour {
 	Vector3 pos;
 	// Use this for initialization
 	void Start () {
-		pos = transform.position;
+		pos = transform.position;	//initial starting position of mob
 	}
 
 	// Update is called once per frame
 	void Update () {
 
+		//gets the distance between the position of the cube and the position fo the player
 		Vector3 direction = target.transform.position - transform.position;
+
+		//gets the distance of how far away the cube is from the initial "spawn point"
 		Vector3 direction2 = pos - transform.position;
+
+		//if player is within range then cube will start following player
 		if ((isFollowing && direction.magnitude <= fRange) || (direction.magnitude <= inRange
-		     && direction2.magnitude <= outRange)) {
+		     && direction2.magnitude <= outRange)) 
+		{
 			isFollowing = true;
-			if( direction.magnitude >= aRange)
+
+			if( direction.magnitude >= aRange)	//keep going towards player until in range for attack
 			{
 				direction.Normalize ();
 				direction *= speed;
 				transform.position += direction;
 			}
-			else if( direction.magnitude < aRange)
+			else if( direction.magnitude < aRange)	//if player is within range, then attack
 			{
 				//attack
 				isFollowing = false;
 			}
 		}
-		else{
+		else
+		{
 			isFollowing = false;
-			if( direction2.magnitude > 1 ){
 
+			if( direction2.magnitude > 1 )	//if not following then head back to initial "spawn point"
+			{
 				direction2.Normalize ();
 				direction2 *= speed;
 				transform.position += direction2;

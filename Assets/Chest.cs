dd7823b77f86afaaf21eb767b0a4744dbc0db24a
mderu <mderu001@ucr.edu>
2015-01-13 09:10:14 -0800

    Slight adjustments and fixes
    Darker willow trees, fixed chest textures, and smoothed terrain.

--- Assets/Chest.cs
+++ Assets/Chest.cs
@@ -3,6 +3,7 @@ using System.Collections;
 
 public class Chest : MonoBehaviour {
 
+    bool chestOpened = false;
 	// Use this for initialization
 	void Start () {
 	
@@ -10,12 +11,19 @@ public class Chest : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	    
+        if (!chestOpened)
+        {
+            transform.GetChild(0).particleSystem.renderer.enabled = false;
+            transform.GetChild(1).gameObject.SetActive(false);
+        }
 	}
 
     void OnTriggerEnter(Collider other){
         if (other.gameObject.layer == Layers.Player)
         {
+            chestOpened = true;
+            transform.GetChild(0).particleSystem.renderer.enabled = true;
+            transform.GetChild(1).gameObject.SetActive(true);
             animation.Play("chestOpen");
             Object.Destroy(collider);
         }

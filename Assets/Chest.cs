45443d6479247ade755e1fc2b59f8ce41c4f8131
mderu <mderu001@ucr.edu>
2015-01-12 17:54:15 -0800

    Added Chest, Organized info

---
+++ Assets/Chest.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Chest : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+
+    void OnTriggerEnter(Collider other){
+        if (other.gameObject.layer == Layers.Player)
+        {
+            animation.Play("chestOpen");
+            Object.Destroy(collider);
+        }
+    }
+}

7308ec778f02f6ea47245cb9184a0fabdf6c6a80
ThatZaZaBean <zazabean@live.com>
2015-01-14 11:35:34 -0800

    Added Aggro State to Enemy  Movement
    Added the aggro state along with other to-do states in the enemy state
    machine within the enemy movement script

--- Assets/EnemyMovement.cs
+++ Assets/EnemyMovement.cs
@@ -15,90 +15,143 @@ public class EnemyMovement : MonoBehaviour {
 	float enemyZ = 0;
 	public float enemyPatrolRadiusUpperBound = 10;
 	public float enemyPatrolRadiusLowerBound = 15;
+	public float enemyAggroRadius = 3;
 	//--------------------------------------------------------------------
-	
-	//Raymond's Variables------------------------------------
-	//float timer = 0;	//time
-	//float speed = 0.01f;	//speed of the mob
-	//bool isFollowing = false;
-	//float fRange = 25f;// follow range
-	//float aRange = 2.25f;//attack range
-	//float outRange = 29f;//out of distance from original position range
-	//float inRange = 10f;//in range of aggro
-	//float dmg_time = 11; //time since taken damage
-	//Vector3 pos;
-	//--------------------------------------------------------
-	// Use this for initialization
-	
-	//enum State{Idle, Attack, AttackAnim, };
-	
-	//State aiState;
-	
+		
 	void Start () {
         enemyPatrolRadius = Random.Range(enemyPatrolRadiusLowerBound, enemyPatrolRadiusUpperBound);
 		enemyInitialSpawn = transform.position;
 		enemyPatrolNext.Set (0, 0, 0);
 		enemyDirection.Set (0, 0, 0);
 	}
-	
+
+	//========================================================================
 	//---------------------Enemy AI State Machine----------------------------
+	//========================================================================
+
+	//----------------Variables and States-------------------
 	float idleTimer = 0;
-	public enum stateEnemyAI {init, patrol, idle, agro, attack};
+	public enum stateEnemyAI {init, patrol, idle, agro, attack, idlePause, returnToPost};
 	private stateEnemyAI EnemyAI = stateEnemyAI.init;
-	
+	//-------------------------------------------------------
+
+	//=================-------State Machine------===================
 	void enemyAIStateMachine() {
 		switch (EnemyAI) { //Transitions
-			
+
+		//========Initial state==========
 		case stateEnemyAI.init:
 			EnemyAI = stateEnemyAI.idle;
 			Debug.Log ("Enemy Patrol Radius: " + enemyPatrolRadius);
 			Debug.Log ("Going to Idle.");
 			break;
-			
+
+		//========Idle State===========
 		case stateEnemyAI.idle:	//idle state
-			if (idleTimer >= 2) {	//then calculate next patrol point and switch states
-				EnemyAI = stateEnemyAI.patrol; //switch states
-				enemyX = enemyInitialSpawn.x + Random.insideUnitCircle.x * enemyPatrolRadius; 
-				enemyZ = enemyInitialSpawn.z + Random.insideUnitCircle.y * enemyPatrolRadius;
-				enemyPatrolNext.Set(enemyX, 0, enemyZ);
+			if( PersonWithinRange(enemyAggroRadius) )
+			{
+				EnemyAI = stateEnemyAI.agro;
+			}
+			else
+			{
+				if (idleTimer >= 1) {	//then calculate next patrol point and switch states
+					EnemyAI = stateEnemyAI.patrol; //switch states
+					enemyX = enemyInitialSpawn.x + Random.insideUnitCircle.x * enemyPatrolRadius; 
+					enemyZ = enemyInitialSpawn.z + Random.insideUnitCircle.y * enemyPatrolRadius;
+					enemyPatrolNext.Set(enemyX, 0, enemyZ);
+					idleTimer = 0;
+					Debug.Log ("Going to Patrol.");
+				}
+				else
+				{
+					idleTimer += Time.deltaTime ; //update timer
+				}
+			}
+			break;
+
+		//=========Patrol State=========
+		case stateEnemyAI.patrol:   //patrol
+			if( PersonWithinRange(enemyAggroRadius) )
+			{
+				EnemyAI = stateEnemyAI.agro;
+				Debug.Log ("Going to Agro");
+			}
+			else
+			{
+				if ((transform.position - enemyPatrolNext).sqrMagnitude < enemySpeed) 
+				{
+					EnemyAI = stateEnemyAI.idle; //switch states
+					Debug.Log ("Going to Idle.");
+				} 
+				else 
+				{
+					//enemy movement action
+					enemyDirection = enemyPatrolNext - transform.position;
+					enemyDirection.Normalize ();
+					enemyDirection *= (enemySpeed * Time.deltaTime);
+					transform.position += enemyDirection;
+				}
+			}
+			break;
+		
+		//=========Agro State==========
+		case stateEnemyAI.agro:	//agro
+			if (!AIWithinRange(enemyPatrolRadius))
+				//if not in range then go back to idle
+				EnemyAI = stateEnemyAI.idlePause;
+			else
+			{
+				//follow the player
+				enemyDirection = target.transform.position - transform.position;
+				enemyDirection.Normalize ();
+				enemyDirection *= (enemySpeed * Time.deltaTime);
+				transform.position += enemyDirection;
+			}
+
+			break;
+
+		//==========Attack State==========
+		case stateEnemyAI.attack:	//NEED TO DO
+			break;
+
+		//=========Idle Pause==============
+		case stateEnemyAI.idlePause:
+			if(idleTimer >= 2)
+			{
+				EnemyAI = stateEnemyAI.idle;
 				idleTimer = 0;
-				Debug.Log ("Going to Patrol.");
 			}
 			else
 			{
 				idleTimer += Time.deltaTime ; //update timer
 			}
 			break;
-			
-		case stateEnemyAI.patrol:   //patrol
-			if ((transform.position - enemyPatrolNext).sqrMagnitude < enemySpeed) 
-			{
-				EnemyAI = stateEnemyAI.idle; //switch states
-				Debug.Log ("Going to Idle.");
-			} 
-			else 
-			{
-				//enemy movement action
-				enemyDirection = enemyPatrolNext - transform.position;
-				enemyDirection.Normalize ();
-				enemyDirection *= (enemySpeed * Time.deltaTime);
-				transform.position += enemyDirection;
-			}
+
+		//============Return to Post State=============
+		case stateEnemyAI.returnToPost:	//NEED TO DO
 			break;
-			
-		case stateEnemyAI.agro:	//agro
-			
-			break;
-			
-		case stateEnemyAI.attack:
-			
-			break;
-			
+
 		default:
 			break;	
 		}
 	}
-	
+	//=======================================================================================
+
+	bool PersonWithinRange(float Range) // Circle equatioooon!
+	{
+		return (Mathf.Pow((target.transform.position.x - transform.position.x), 2) +
+		        Mathf.Pow((target.transform.position.y - transform.position.y), 2)) <= Mathf.Pow(Range, 2);
+	}
+
+	bool AIWithinRange(float Range) // Circle equatioooon!
+	{
+		return (Mathf.Pow((transform.position.x - enemyInitialSpawn.x), 2) +
+		        Mathf.Pow((transform.position.y - enemyInitialSpawn.y), 2)) <= Mathf.Pow(Range, 2);
+	}
+
+	//==========================================================================================
+	//--------------------------------------UPDATE FUNCTION-------------------------------------
+	//==========================================================================================
 	// Update is called once per frame
 	void Update () {
 		

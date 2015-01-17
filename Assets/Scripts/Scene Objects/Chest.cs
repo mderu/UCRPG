7690a4c85eaa99cbf080595f001cf3d9820e68b8
mderu <mderu001@ucr.edu>
2015-01-17 07:34:26 -0800

    Fixed wall lighting and water cannot be pathed

---
+++ Assets/Scripts/Scene Objects/Chest.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Chest : MonoBehaviour {
+
+    bool chestOpened = false;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (!chestOpened)
+        {
+            transform.GetChild(0).particleSystem.renderer.enabled = false;
+            transform.GetChild(1).gameObject.SetActive(false);
+        }
+	}
+
+    void OnTriggerEnter(Collider other){
+        if (other.gameObject.layer == Layers.Player)
+        {
+            chestOpened = true;
+            transform.GetChild(0).particleSystem.renderer.enabled = true;
+            transform.GetChild(1).gameObject.SetActive(true);
+            animation.Play("chestOpen");
+            Object.Destroy(collider);
+        }
+    }
+}
---
+++ Assets/Scripts/Scene Objects/WaterWorkAround.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WaterWorkAround : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        BoxCollider[] colliders = gameObject.GetComponentsInChildren<BoxCollider>();
+        for (int i = 0; i < colliders.Length; i++)
+        {
+            colliders[i].isTrigger = true;
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}

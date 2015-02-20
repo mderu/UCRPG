dabce8e0f9e7f32a4c7a968a4726ad1919a3550f
TheBoop <davidxyding1@gmail.com>
2015-02-20 12:30:05 -0800

    Some food
    Looks like Meat is back on the menu boys

---
+++ Assets/Scripts/Rotate.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotate : MonoBehaviour {
+	void Start () {
+		transform.Rotate (Vector3.right, 50f);
+	}
+
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate (Vector3.up, 1f);
+	}
+}
---
+++ Assets/Scripts/pickUp.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class pickUp : MonoBehaviour {
+	void OnTriggerEnter (Collider other){
+		if (other.gameObject.layer == Layers.Player) {
+			Destroy(this.gameObject);
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
